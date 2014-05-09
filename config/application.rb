7c5a6f073c7bcdd79b935c3b39ca99bca3d0bfa3
Seah Ying Cong <sycurfriend00@gmail.com>
2014-05-10 00:50:16 +0800

    Edited precompile line in applications.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Pinteresting
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   #required for heroku
-  end
   config.assets.initialize_on_precompile = false
+  end
 end
